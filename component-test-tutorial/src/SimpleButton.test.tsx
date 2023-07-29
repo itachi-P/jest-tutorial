976b2453be0dee96fc3f14c9641a6d125a46abb7
itachi-P <itachip38@gmail.com>
2023-07-30 08:50:14 +0900

    update: add testing snapshot-test

--- component-test-tutorial/src/SimpleButton.test.tsx
+++ component-test-tutorial/src/SimpleButton.test.tsx
@@ -1,12 +1,24 @@
+// Jest単体ではReactコンポーネントを描画できないので、React Testing Libraryを使用
 import { render, screen } from "@testing-library/react";
+// ボタンのクリックなどユーザーの操作をシミュレートするために必要
 import userEvent from "@testing-library/user-event";
+// テスト対象のコンポーネント
 import { SimpleButton } from "./SimpleButton";
- 
+
+// ユーザーの操作をシミュレートするテスト
 test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
-    const user = userEvent.setup();
-    render(<SimpleButton />);
-    const simpleButton = screen.getByRole("button");
-    expect(simpleButton).toHaveTextContent("OFF");
-    await user.click(simpleButton);
-    expect(simpleButton).toHaveTextContent("ON");
+	const user = userEvent.setup();
+	render(<SimpleButton />);
+	// 初期状態ではOFFと表示されていることの確認
+	const simpleButton = screen.getByRole("button");
+	expect(simpleButton).toHaveTextContent("OFF");
+	// ボタンのラベルがOFFの状態でクリックされたらONになる
+	await user.click(simpleButton);
+	expect(simpleButton).toHaveTextContent("ON");
+});
+
+// スナップショットテスト - 描画直後の状態をテストする
+test("描画されてすぐはOFFと表示されている", () => {
+	const view = render(<SimpleButton />);
+	expect(view.container).toMatchSnapshot();
 });
